57738cbac5e75871f0e42ace46d9a55e79834750
csastudentcb1 <csastudentcb1@gmail.com>
2026-02-28 13:04:33 -0800

    coded attacking, and animations
    2/28/26 1:04 pm

--- Assets/_Prototype/scripts/player.cs
+++ Assets/_Prototype/scripts/player.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D), typeof(Animator))]
 
 public class player : MonoBehaviour
 {
+    public int damage;
+    public int health;
+    public int attackspeed;
+    public BoxCollider2D hitbox;
     public float maxspeed;
     public float moveForce;
     Rigidbody2D rb;
     Animator anim;
 
-
+    float attackCoolDownTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -22,43 +27,58 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        move();
+
+
+        if (Input.GetMouseButtonDown(0)  && attackCoolDownTimer >= attackspeed)
+        {
+            attack();
+            attackCoolDownTimer = 0;
+        }
+
+
+        attackCoolDownTimer += Time.deltaTime;
+
+
+    }
+
+    private void attack()
+    {
+
+        // attaxck
+        var damageable = FindObjectsByType<GameObject>(FindObjectsInactive.Exclude, FindObjectsSortMode.None)
+            .Where(x => x.TryGetComponent(out IDamageable damageable));
+
+        GameObject target = damageable.FirstOrDefault(x => hitbox.bounds.Contains(x.transform.position));
+
+        if (target != null)
+        {
+            Vector2 dirtotarget = (target.transform.position - transform.position).normalized;
+            target.GetComponent<IDamageable>().TakeDamage(dirtotarget, damage);
+        }
+
+
+        anim.SetTrigger("attack");
+    }
+
+    private void move()
+    {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
 
 
-        Vector2 motion = new Vector2(x,y) * moveForce * Time.fixedDeltaTime;
+        Vector2 motion = new Vector2(x, y) * moveForce * Time.fixedDeltaTime;
         rb.velocity = motion;
 
-        anim.SetInteger("x", (int) x);
+        anim.SetInteger("x", (int)x);
 
-        anim.SetInteger ("y", (int) y);
+        anim.SetInteger("y", (int)y);
 
 
         if ((int)x != 0)
         {
             anim.SetInteger("y", (int)0);
         }
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
